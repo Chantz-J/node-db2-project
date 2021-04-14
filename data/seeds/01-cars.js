72f42768c34d4c011707df826feb2fea1757c0ee
Chantz-J <johnson.chantz@gmail.com>
2021-04-13 20:40:48 -0400

    Added a seed to table.

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
 // STRETCH
+exports.seed = function(knex){
+    //This command deletes all existing entires
+    return knex('cars').truncate()
+    .then(function () {
+        //Then inser entries 
+        return knex('cars').insert([
+            {vin: 1010101010101010, make: 'Kia', model: 'Soul', mileage: 250.000, title: 'Carvana', transmission: 'car tingz'}
+        ])
+    })
+}
